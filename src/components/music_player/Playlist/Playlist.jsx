44e9a84bf5e9bc97b29b487c8b7ecdc23ab90fd8
Peter Pitzulo <plpitzulo@protonmail.com>
2023-05-24 19:13:33 -0400

    Basic playlist structure

--- src/components/music_player/Playlist/Playlist.jsx
+++ src/components/music_player/Playlist/Playlist.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './Playlist.css'
 
 function Playlist() {
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+
+
   return (
-    <div>Playlist</div>
+    <div className="playlist-container">
+      <div className="header">
+        Header
+      </div>
+      <div className="playlist">
+        <div className="song">
+          <div className="test">
+            <img className="thumbnail2" src="img/akatsuki_records.jpg" alt="thumbnail" />
+            <div>
+              <p>Bloody Devotion</p>
+              <p>Akatsuki Records</p>
+            </div>
+          </div>
+          <p>3:48</p>
+          {/* <span className="duration">{formatTime(currentTime)} / {formatTime(duration)}</span> */}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/music_player/music_player.jsx
+++ src/components/music_player/music_player.jsx
@@ -1,39 +1,47 @@
 import React, { useState, useRef, useEffect } from 'react'
 import './music_player.css'
+import Playlist from './Playlist/Playlist'
 
 function music_player() {
-    const [currentTime, setCurrentTime] = useState(0)
-    const [duration, setCurrentDuration] = useState(0)
-    const [isPlaying, setIsPlaying] = useState(false)
-    const audioPlayerRef = useRef(null)
-    
-    useEffect(() => {
-      // Update the duration state when the audio is loaded
-      audioPlayerRef.current.addEventListener('loadedmetadata', () => {
-        setCurrentDuration(audioPlayerRef.current.duration)
-      })
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setCurrentDuration] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioPlayerRef = useRef(null)
   
-      // Update the currentTime state during playback
-      audioPlayerRef.current.addEventListener('timeupdate', () => {
-        setCurrentTime(audioPlayerRef.current.currentTime)
-      })
-    }, [])
+  useEffect(() => {
+    // Update the duration state when the audio is loaded
+    audioPlayerRef.current.addEventListener('loadedmetadata', () => {
+      setCurrentDuration(audioPlayerRef.current.duration)
+    })
 
-    const play = () => {
-      audioPlayerRef.current.play()
-      setIsPlaying(true)
-    }
+    // Update the currentTime state during playback
+    audioPlayerRef.current.addEventListener('timeupdate', () => {
+      setCurrentTime(audioPlayerRef.current.currentTime)
+    })
+  }, [])
 
-    const pause = () => {
-      audioPlayerRef.current.pause()
-      setIsPlaying(false)
-    }
 
-    const handleSeek = (e) => {
-      const seekTime = parseFloat(e.target.value);
-      audioPlayerRef.current.currentTime = seekTime;
-      setCurrentTime(seekTime);
-    };
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  const play = () => {
+    audioPlayerRef.current.play()
+    setIsPlaying(true)
+  }
+
+  const pause = () => {
+    audioPlayerRef.current.pause()
+    setIsPlaying(false)
+  }
+
+  const handleSeek = (e) => {
+    const seekTime = parseFloat(e.target.value);
+    audioPlayerRef.current.currentTime = seekTime;
+    setCurrentTime(seekTime);
+  };
 
   return (
     <div id="music_player_container">
@@ -44,10 +52,12 @@ function music_player() {
             alt="Yuuka album art"
           />
         </div>
-        <audio src="http://localhost:8000/media/audio/necromantic_7BBsJib.opus" ref={audioPlayerRef} ></audio>
+        <Playlist />
+        <audio src="http://localhost:8000/media/audio/Emblem.opus" ref={audioPlayerRef} ></audio>
         <div className="player background">
           {/* <input type="range" id="volume-slider" max="100" value="100" /> */}
           <input type="range" id="seek-slider" min="0" step="0.01" max={duration} value={currentTime} onChange={handleSeek} />
+          <span className="duration">{formatTime(currentTime)} / {formatTime(duration)}</span>
           <div className="control-buttons">
             <div className="controls">
               <button id="previous"><i className="fa-solid fa-backward"></i></button>
